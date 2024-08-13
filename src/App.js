6d4f49e89e10b8801914942de5e9bc98cca2feeb
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2024-08-13 10:23:43 +0700

    form & preview image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "bulma/css/bulma.css"
 import ProductList from "./components/ProductList";
+import AddProduct from "./components/addProduct";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProductList/>}/>
+        <Route path="/add" element={<AddProduct/>}/>
       </Routes>
     </BrowserRouter>
   );
